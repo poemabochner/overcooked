38a77b2e0a2ff1fe53b08cf9bd788007eacf7323
Poema Bochner <poemabochner8@gmail.com>
2023-12-29 14:03:12 -0300

    criando base do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Meu primeiro projeto Vue!</h1>
+  <header class="banner">
+    <div class="apresentacao">
+      <img src="./assets/images/overcooked.svg" style="width: 20%; filter: invert(100%) sepia(3%) saturate(12%) hue-rotate(103deg) brightness(105%) contrast(105%);" alt="logo do overcooked" class="logo">
+      <p class="cabecalho-lh frase-cabecalho">
+        <span class="texto">um banquete de ideias para</span>
+        despertar o chef que há em você!
+      </p>
+
+      <p class="subtitulo-lg">
+        explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+      </p>
+    </div>
+    <img src="./assets/images/foto-banner.png" alt="" class="foto-banner">
+  </header>
 </template>
