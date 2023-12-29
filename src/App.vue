d82c71443c021cf9ff3a0b7a8326dd61d74a7f9b
Poema Bochner <poemabochner8@gmail.com>
2023-12-29 14:12:08 -0300

    criando primeiro componente

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <header class="banner">
-    <div class="apresentacao">
-      <img src="./assets/images/overcooked.svg" style="width: 20%; filter: invert(100%) sepia(3%) saturate(12%) hue-rotate(103deg) brightness(105%) contrast(105%);" alt="logo do overcooked" class="logo">
-      <p class="cabecalho-lh frase-cabecalho">
-        <span class="texto">um banquete de ideias para</span>
-        despertar o chef que há em você!
-      </p>
-
-      <p class="subtitulo-lg">
-        explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
-      </p>
-    </div>
-    <img src="./assets/images/foto-banner.png" alt="" class="foto-banner">
-  </header>
+  <Banner/>
 </template>
+
+<script lang="ts">
+  import Banner from './components/Banner.vue'
+
+  export default {
+    components: {
+      Banner: Banner
+    }
+  }
+</script>
