c5baaded8a981e9f158a700521c13a6be8a9bd5d
Poema Bochner <poemabochner8@gmail.com>
2024-01-29 20:30:18 -0300

    cria prop de estilo

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -1,27 +1,36 @@
 <template>
-  <span class="tag">
+  <span class="tag" :class="{ ativa }">
     {{ texto }}
   </span>
 </template>
 
 <script lang="ts">
-  export default {
-    props: {
-      texto: {
-        type: String, required: true
-      }
+export default {
+  props: {
+    texto: {
+      type: String, required: true
+    },
+    ativa: {
+      type: Boolean
     }
   }
+}
 </script>
 
 <style scoped>
-  .tag {
+.tag {
   display: inline-block;
   border-radius: 0.5rem;
   min-width: 4.25rem;
   padding: 0.5rem;
   text-align: center;
   transition: 0.2s;
+  color: var(--cinza);
+  background: var(--cinza-claro);
+  font-weight: 400;
+}
+
+.tag.ativa {
   color: var(--creme, #FFFAF3);
   background: var(--pink);
   font-weight: 700;
