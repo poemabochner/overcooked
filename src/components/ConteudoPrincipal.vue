c688797057de37a87278aaac68a77f1257e09898
Poema Bochner <poemabochner8@gmail.com>
2024-01-29 20:31:07 -0300

    remove elementos da SuaLista e usa o componente

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,88 +1,21 @@
-<template>
-  <main class="conteudo-principal">
-    <section>
-      <span class="subtitulo-lg sua-lista-texto">
-        sua lista:
-      </span>
-
-      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente" class="">
-          <Tag :texto="ingrediente"></Tag>
-        </li>
-      </ul>
-
-      <p v-else class="paragrafo lista-vazia">
-        <img src="../assets/images/icones/lista-vazia.svg" />
-        sua lista está vazia, selecione ingredientes para adicionar
-      </p>
-    </section>
-
-    <selecionar-ingredientes />
-  </main>
-</template>
-
 <script lang="ts">
-import SelecionarIngredientes from './SelecionarIngredientes.vue'
-import Tag from './Tag.vue'
+import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import SuaLista from './SuaLista.vue';
+
 export default {
-  components: { SelecionarIngredientes, Tag },
   data() {
     return {
-      ingredientes: ['alho', 'manteiga', 'orégano']
-    }
-  }
+      ingredientes: ['Alho', 'Manteiga', 'Orégano']
+    };
+  },
+  components: { SelecionarIngredientes, SuaLista }
 }
 </script>
 
-<style scoped>
-.conteudo-principal {
-  padding: 6.5rem 7.5rem;
-  border-radius: 3.75rem 3.75rem 0rem 0rem;
-  background: var(--creme, #FFFAF3);
-  color: var(--cinza, #444);
+<template>
+  <main class="conteudo-principal">
+    <SuaLista :ingredientes="ingredientes" />
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 5rem;
-}
-
-.sua-lista-texto {
-  color: var(--pink);
-  display: block;
-  text-align: center;
-  margin-bottom: 1.5rem;
-}
-
-.ingredientes-sua-lista {
-  display: flex;
-  justify-content: center;
-  gap: 1rem 1.5rem;
-  flex-wrap: wrap;
-}
-
-.lista-vazia {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 0.25rem;
-
-  color: var(--coral, #F0633C);
-  text-align: center;
-}
-
-@media only screen and (max-width: 1300px) {
-  .conteudo-principal {
-    padding: 5rem 3.75rem;
-    gap: 3.5rem;
-  }
-}
-
-@media only screen and (max-width: 767px) {
-  .conteudo-principal {
-    padding: 4rem 1.5rem;
-    gap: 4rem;
-  }
-}
-</style>
+    <SelecionarIngredientes />
+  </main>
+</template>
